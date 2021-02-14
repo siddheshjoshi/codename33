08c94833ca4537bb9b177d7d3322b5b26e277513
Siddhesh Joshi <siddheshmail@gmail.com>
2021-02-14 18:55:55 -0500

    add identity

--- src/main.js
+++ src/main.js
@@ -2,6 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import netlifyIdentity from 'netlify-identity-widget'
+
+if (window.location.href.indexOf('localhost') > -1) {
+  netlifyIdentity.init({
+    APIUrl: '/.netlify/identity',
+    logo: false // you can try false and see what happens
+  })
+} else {
+  netlifyIdentity.init({
+    APIUrl: 'https://fervent-ardinghelli-a38315.netlify.app/.netlify/identity',
+    logo: true // you can try false and see what happens
+  })
+}
 
 Vue.config.productionTip = false
 
