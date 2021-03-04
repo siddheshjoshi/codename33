4e461d3a8e7153d6767737e2443eca5c899bb5f5
Rishi <rishikesh.a.j@gmail.com>
2021-03-04 16:37:37 -0500

    changes in functions

--- src/views/WelcomePage.vue
+++ src/views/WelcomePage.vue
@@ -1,18 +1,82 @@
 <template>
   <div class="about">
-    <h1>This is a welcome page</h1>
-    <button @click="logout">Logout</button>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <button class="btn btn-outline-primary mt-3" @click="logout">Logout</button>
+        </div>
+      </div>
+
+      <div class="row justify-content-center mt-5">
+        <div class="col-md-9">
+          <div class="input-group">
+            <input type="text" v-model="text" class="form-control" aria-label="Text input with checkbox">
+            <input type="button" class="btn btn-outline-secondary ml-3" @click="onSave()" value="+" aria-label="Add">
+          </div>
+        </div>
+      </div>
+
+      <div class="row justify-content-center mt-5" v-for="item,index in todoArray" :key="index">
+        <div class="col-md-9">
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <div class="input-group-text">
+                <input type="checkbox" v-model="isChecked" value="item.isChecked" aria-label="Checkbox for following text input">
+              </div>
+            </div>
+                <input type="text" v-model="item.text" class="form-control" aria-label="Text input with checkbox">
+                <input type="button" class="btn btn-outline-primary ml-1" value="Save" aria-label="Save">
+                <input type="button" class="btn btn-outline-primary ml-1" value="Delete" aria-label="Delete">
+                <input type="button" class="btn btn-outline-secondary ml-3 add-btn" value="+" aria-label="Save">
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import { mapActions } from 'vuex'
+import axios from "axios";
 
 export default {
+  
   methods: {
     ...mapActions('user', {
       logout: 'logout'
-    })
+    }),
+    onSave(){
+      this.todoArray.push({isChecked:false,text: this.text});
+      console.log(this.todoArray)
+      let baseURL = 'https://trusting-heisenberg-236a0c.netlify.app/.netlify/functions/todos-create';
+      axios.post(baseURL, this.todoArray).then((response)=>{
+        console.log(response);
+      })
+    }
+  },
+  data : function(){
+    return{
+      todoArray: [
+        {isChecked : false, text : 'todo number one'},
+        {isChecked : false, text : 'todo number two'},
+        {isChecked : false, text : 'todo number three'},
+      ],
+      isChecked: false, 
+      text: ''
+    }
   }
 }
 </script>
+
+<style scoped>
+  .add-btn{
+    border-radius: 100%;
+    color: white  !important;
+    background: grey;
+    position: fixed;
+    bottom: 10px;
+    right: 10px; 
+    width: 60px;
+    height: 60px;
+  }
+</style>
